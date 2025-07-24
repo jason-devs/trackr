2474f8bd6ac2e257da6bee49e530cb178241c51a
jason-devs <jasondevs.mail@gmail.com>
2025-07-24 12:20:28 +0100

    feature: add workout page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Dashboard from "./pages/Dashboard";
+import Workout from "./pages/Workout";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route index element={<Landing />} />
         <Route path="dashboard" element={<Dashboard />} />
+        <Route path="workout" element={<Workout />} />
       </Routes>
     </BrowserRouter>
   );
